1675dcb45ffeda8c2067e014223ba844a4a6274a
ljedrz <ljedrz@gmail.com>
2017-04-08 15:18:57 +0200

    Add combinator tests

--- src/combinators.rs
+++ src/combinators.rs
@@ -26,10 +26,10 @@ pub fn k() -> Term { abs(abs(Var(2))) }
 /// S := λxyz.x z (y z) = λ λ λ 3 1 (2 1)
 pub fn s() -> Term { abs(abs(abs(Var(3).app(Var(1)).app(Var(2).app(Var(1)))))) }
 
-/// X - the Iota combinator.
+/// Iota - the universal combinator.
 ///
-/// X := λx.x S K = λ 1 S K
-pub fn x() -> Term { abs(Var(1).app(k()).app(s())) }
+/// ι := λx.x S K = λ 1 S K
+pub fn iota() -> Term { abs(Var(1).app(s()).app(k())) }
 
 /// B - the composition combinator.
 ///
@@ -66,3 +66,63 @@ pub fn omm() -> Term { om().app(om()) }
 /// Y := λg.(λx.g (x x)) (λx.g (x x)) = λ (λ 2 (1 1)) (λ 2 (1 1))
 pub fn y() -> Term { abs(app(abs(Var(2).app(Var(1).app(Var(1)))), abs(Var(2).app(Var(1).app(Var(1)))))) }
 
+#[cfg(test)]
+mod test {
+	use super::*;
+	use arithmetic::*;
+//	use pair::*;
+	use reduction::*;
+
+	#[test]
+	fn ski_tests() {
+		assert_eq!(normalize(i().app(zero())), zero());
+		assert_eq!(normalize(k().app(zero()).app(one())), zero());
+		assert_eq!(normalize(s().app(zero()).app(one()).app(to_cnum(2))),
+				   normalize(zero().app(to_cnum(2)).app(one().app(to_cnum(2)))));
+	}
+
+	#[test]
+	fn iota_tests() {
+		assert_eq!(normalize(iota().app(iota())), i());
+		assert_eq!(normalize(iota().app(iota().app(iota().app(iota())))), k());
+		assert_eq!(normalize(iota().app(iota().app(iota().app(iota().app(iota()))))), s());
+	}
+
+	#[test]
+	fn bckw_tests() {
+		assert_eq!(normalize(b().app(zero()).app(one()).app(to_cnum(2))),
+				   normalize(zero().app(one().app(to_cnum(2)))));
+		assert_eq!(normalize(c().app(zero()).app(one()).app(to_cnum(2))),
+				   normalize(zero().app(to_cnum(2)).app(one())));
+		assert_eq!(normalize(w().app(zero()).app(one())),
+				   normalize(zero().app(one()).app(one())));
+	}
+
+	#[test]
+	fn omega_tests() {
+		assert_eq!(normalize(om().app(zero())), normalize(zero().app(zero())));
+//		assert_eq!(normalize(omm()), omm()); TODO
+	}
+/*
+	#[test]
+	fn fixed_tests() {
+		// Y (λgqab. LT a b (PAIR q a) (g (SUCC q) (SUB a b) b)) 0
+		let div = y().app(
+			abs(abs(abs(abs(
+				lt()
+				.app(Var(2))
+				.app(Var(1))
+				.app(pair().app(Var(3)).app(Var(2)))
+				.app(
+					Var(4)
+					.app(succ().app(Var(3)))
+					.app(sub().app(Var(2)).app(Var(1)))
+					.app(Var(1))
+				)
+			))))
+		).app(zero());
+
+		assert_eq!(normalize(div.app(to_cnum(6)).app(to_cnum(3))),
+				   normalize(pair().app(to_cnum(2)).app(zero())));
+	}*/
+}
