81195dc8180d9e928f9ba353ef6599ab8f93f772
ljedrz <ljedrz@gmail.com>
2017-04-26 10:44:41 +0200

    Move minor reducing functions to reduction.rs

--- src/reduction.rs
+++ src/reduction.rs
@@ -70,10 +70,96 @@ pub fn normalize(term: Term) -> Term {
     nf(0, term, VecDeque::new())
 }
 
+/// Applies two terms with substitution and variable update, consuming them in the process.
+///
+/// # Example
+/// ```
+/// use lambda_calculus::term::apply;
+/// use lambda_calculus::parser::parse;
+///
+/// let lhs    = parse(&"λλ42(λ13)").unwrap();
+/// let rhs    = parse(&"λ51").unwrap();
+/// let result = parse(&"λ3(λ61)(λ1(λ71))").unwrap();
+///
+/// assert_eq!(apply(lhs, rhs), Ok(result));
+/// ```
+pub fn apply(mut lhs: Term, rhs: Term) -> Result<Term, Error> {
+    _apply(&mut lhs, rhs, 0);
+
+    lhs.unabs()
+}
+
+fn _apply(lhs: &mut Term, rhs: Term, depth: usize) {
+    match *lhs {
+        Var(i) => if i == depth {
+            *lhs = rhs; // substitute a top-level variable from lhs with rhs
+            update_free_variables(lhs, depth - 1, 0); // update indices of free variables from rhs
+        } else if i > depth {
+            *lhs = Var(i - 1) // decrement a free variable's index
+        },
+        Abs(_) => {
+            _apply(lhs.unabs_ref_mut().unwrap(), rhs, depth + 1)
+        },
+        App(_, _) => {
+            _apply(lhs.lhs_ref_mut().unwrap(), rhs.clone(), depth);
+            _apply(lhs.rhs_ref_mut().unwrap(), rhs, depth)
+        }
+    }
+}
+
+fn update_free_variables(term: &mut Term, added_depth: usize, own_depth: usize) {
+    match *term {
+        Var(i) => if i > own_depth {
+            *term = Var(i + added_depth)
+        },
+        Abs(_) => {
+            update_free_variables(term.unabs_ref_mut().unwrap(), added_depth, own_depth + 1)
+        },
+        App(_, _) => {
+            update_free_variables(term.lhs_ref_mut().unwrap(), added_depth, own_depth);
+            update_free_variables(term.rhs_ref_mut().unwrap(), added_depth, own_depth)
+        }
+    }
+}
+
 /// Set to `true` to see all the steps of β-reductions. The default is `false`.
 pub const SHOW_REDUCTIONS: bool = false;
 
 impl Term {
+    /// Applies `self` to another term with substitution and variable update, consuming them
+    /// in the process.
+    ///
+    /// # Example
+    /// ```
+    /// use lambda_calculus::parser::parse;
+    ///
+    /// let lhs    = parse(&"λλ42(λ13)").unwrap();
+    /// let rhs    = parse(&"λ51").unwrap();
+    /// let result = parse(&"λ3(λ61)(λ1(λ71))").unwrap();
+    ///
+    /// assert_eq!(lhs.apply(rhs), Ok(result));
+    /// ```
+    pub fn apply(self, rhs: Term) -> Result<Term, Error> {
+        apply(self, rhs)
+    }
+
+    /// Reduces an `App`lication by substitution and variable update.
+    ///
+    /// # Example
+    /// ```
+    /// use lambda_calculus::term::{app, abs};
+    /// use lambda_calculus::term::Term::Var;
+    /// use lambda_calculus::arithmetic::zero;
+    /// use lambda_calculus::combinators::i;
+    ///
+    /// assert_eq!(app(i(), zero()).eval(), Ok(abs(abs(Var(1)))));
+    /// ```
+    pub fn eval(self) -> Result<Term, Error> {
+        let (lhs, rhs) = try!(self.unapp());
+
+        apply(lhs, rhs)
+    }
+
     /// Performs a single normal-order β-reduction on `self`.
     ///
     /// # Example
