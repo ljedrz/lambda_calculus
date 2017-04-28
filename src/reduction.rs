f9e57a6f23ffbe95e6c04fa5518aac2d0e152cc3
ljedrz <ljedrz@gmail.com>
2017-04-28 14:30:34 +0200

    fully deprecate old normalization code

--- src/reduction.rs
+++ src/reduction.rs
@@ -1,78 +1,8 @@
 //! [β-reduction](https://en.wikipedia.org/wiki/Beta_normal_form) for lambda `Term`s
 
-#![allow(dead_code)]
-
 use term::*;
 use term::Term::*;
-/*
-use self::Closure::*;
-use self::Expression::*;
-use std::collections::VecDeque;
 
-#[derive(Debug, PartialEq, Clone)]
-enum Closure {
-    Depth(usize),
-    TermInEnv(Term, Environment)
-}
-
-#[derive(Debug, PartialEq)]
-enum Expression {
-    Evaluation(Closure),
-    Application(Box<Expression>, Term, Environment)
-}
-
-type Environment = VecDeque<Closure>;
-
-fn whnf(term: Term, mut env: Environment) -> Expression {
-    match term {
-        Var(i) => match env.remove(i - 1) {
-            Some(Depth(i)) => Evaluation(Depth(i)),
-            Some(TermInEnv(t, e)) => whnf(t, e),
-            None => unreachable!()
-        },
-        Abs(t) => Evaluation(TermInEnv(Abs(t), env)),
-        App(lhs, rhs) => match whnf(*lhs, env.clone()) { // TODO: try to remove this clone()
-            Evaluation(TermInEnv(Abs(lhs2), mut env2)) => {
-                env2.push_front(TermInEnv(*rhs, env));
-                whnf(*lhs2, env2)
-            },
-            expr => Application(Box::new(expr), *rhs, env)
-        }
-    }
-}
-
-fn reduce(depth: usize, exp: Expression) -> Term {
-    match exp {
-        Application(lhs, rhs, env) => app(reduce(depth, *lhs), nf(depth, rhs, env)),
-        Evaluation(TermInEnv(Abs(term), mut env)) => {
-            env.push_front(Depth(depth));
-            abs(nf(depth + 1, *term, env))
-        },
-        Evaluation(TermInEnv(term, _)) => term,
-        Evaluation(Depth(i)) => Var(depth - i)
-    }
-}
-
-fn nf(depth: usize, term: Term, env: Environment) -> Term {
-    reduce(depth, whnf(term, env))
-}
-
-/// Returns a term reduced to its normal form. Consumes its argument.
-///
-/// # Example
-///
-/// ```
-/// use lambda_calculus::booleans::{if_else, tru};
-/// use lambda_calculus::arithmetic::{zero, one, succ};
-/// use lambda_calculus::reduction::normalize;
-///
-/// assert_eq!(normalize(succ().app(zero())), one());
-/// assert_eq!(normalize(if_else().app(tru()).app(one()).app(zero())), one());
-/// ```
-fn normalize(term: Term) -> Term {
-    nf(0, term, VecDeque::new())
-}
-*/
 /// Applies two terms with substitution and variable update, consuming the first one in the process.
 ///
 /// # Example
