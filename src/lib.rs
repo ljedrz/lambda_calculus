f7e1938b3b93376dba667717cc7eebbe4f89f0c8
ljedrz <ljedrz@gmail.com>
2017-06-06 11:03:02 +0200

    adhere to C-REEXPORT

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,10 @@ pub mod reduction;
 pub mod combinators;
 #[cfg(not(feature = "no_church"))]
 pub mod church;
+
+pub use self::term::{Term, abs, app};
+pub use self::term::Term::*;
+pub use self::term::Notation::*;
+pub use self::reduction::beta;
+pub use self::reduction::Order::*;
+pub use self::parser::parse;
