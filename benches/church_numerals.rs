562468187f84b5e84e8743b0b73a867590c634fb
ljedrz <ljedrz@gmail.com>
2017-12-07 13:58:16 +0100

    add more benches

--- benches/church_numerals.rs
+++ benches/church_numerals.rs
@@ -54,6 +54,56 @@ fn remainder(b: &mut Bencher) {
 }
 
 #[bench]
+fn compare_eq(b: &mut Bencher) {
+    b.iter(|| { beta(app!(eq(), 3.into(), 2.into()), HAP, 0, false) } );
+}
+
+#[bench]
+fn compare_neq(b: &mut Bencher) {
+    b.iter(|| { beta(app!(neq(), 3.into(), 2.into()), HAP, 0, false) } );
+}
+
+#[bench]
+fn compare_gt(b: &mut Bencher) {
+    b.iter(|| { beta(app!(gt(), 3.into(), 2.into()), HAP, 0, false) } );
+}
+
+#[bench]
+fn compare_lt(b: &mut Bencher) {
+    b.iter(|| { beta(app!(lt(), 3.into(), 2.into()), HAP, 0, false) } );
+}
+
+#[bench]
+fn compare_geq(b: &mut Bencher) {
+    b.iter(|| { beta(app!(geq(), 3.into(), 2.into()), HAP, 0, false) } );
+}
+
+#[bench]
+fn compare_leq(b: &mut Bencher) {
+    b.iter(|| { beta(app!(leq(), 3.into(), 2.into()), HAP, 0, false) } );
+}
+
+#[bench]
+fn minimum(b: &mut Bencher) {
+    b.iter(|| { beta(app!(min(), 3.into(), 2.into()), HAP, 0, false) } );
+}
+
+#[bench]
+fn maximum(b: &mut Bencher) {
+    b.iter(|| { beta(app!(max(), 3.into(), 2.into()), HAP, 0, false) } );
+}
+
+#[bench]
+fn left_shift(b: &mut Bencher) {
+    b.iter(|| { beta(app!(lshift(), 3.into(), 2.into()), HAP, 0, false) } );
+}
+
+#[bench]
+fn right_shift(b: &mut Bencher) {
+    b.iter(|| { beta(app!(rshift(), 3.into(), 2.into()), HAP, 0, false) } );
+}
+
+#[bench]
 fn factorial(b: &mut Bencher) {
     b.iter(|| { beta(app!(fac(), 3.into()), HAP, 0, false) } );
 }
