487baed1585df9583d821eaa085e59f3dc00c9ae
ljedrz <ljedrz@gmail.com>
2017-12-14 15:32:16 +0100

    fix one old conversion

--- benches/church_option.rs
+++ benches/church_option.rs
@@ -23,5 +23,5 @@ fn church_is_some(b: &mut Bencher) {
 
 #[bench]
 fn church_map_or(b: &mut Bencher) {
-    b.iter(|| { beta(app!(map_or(), 0.into(), succ(), none()), HAP, 0) } );
+    b.iter(|| { beta(app!(map_or(), 0.into_church(), succ(), none()), HAP, 0) } );
 }
