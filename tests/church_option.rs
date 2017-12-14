9cd3f5eae23acdfa5098a16d180b75787431e7c7
Bill Murphy <billpmurphy92@gmail.com>
2017-12-14 11:06:45 -0500

    add into_church impl for Option<T>

--- tests/church_option.rs
+++ tests/church_option.rs
@@ -8,7 +8,7 @@ use lambda::church::numerals::succ;
 
 #[test]
 fn church_none() {
-    assert_eq!(beta(None.into_church(), HAP, 0), none());
+    assert_eq!(beta(none(), HAP, 0), none());
 }
 
 #[test]
@@ -18,24 +18,24 @@ fn church_some() {
 
 #[test]
 fn church_is_some() {
-    assert_eq!(beta(app(is_some(), None.into_church()), HAP, 0), false.into_church());
+    assert_eq!(beta(app(is_some(), none()), HAP, 0), false.into_church());
     assert_eq!(beta(app(is_some(), Some(3.into_church()).into_church()), HAP, 0), true.into_church());
 }
 
 #[test]
 fn church_is_none() {
-    assert_eq!(beta(app(is_none(), None.into_church()), HAP, 0), true.into_church());
-    assert_eq!(beta(app(is_none(), Some(3.into_church()).into_church()), HAP, 0), false.into_church());
+    assert_eq!(beta(app(is_none(), none()), HAP, 0), true.into_church());
+    assert_eq!(beta(app(is_none(), Some(3).into_church()), HAP, 0), false.into_church());
 }
 
 #[test]
 fn church_map_or() {
-    assert_eq!(beta(app!(map_or(), 5.into_church(), succ(), None.into_church()), HAP, 0), 5.into_church());
-    assert_eq!(beta(app!(map_or(), 5.into_church(), succ(), Some(1.into_church()).into_church()), HAP, 0), 2.into_church());
+    assert_eq!(beta(app!(map_or(), 5.into_church(), succ(), none()), HAP, 0), 5.into_church());
+    assert_eq!(beta(app!(map_or(), 5.into_church(), succ(), Some(1).into_church()), HAP, 0), 2.into_church());
 }
 
 #[test]
 fn church_unwrap_or() {
-    assert_eq!(beta(app!(unwrap_or(), 5.into_church(), None.into_church()), HAP, 0), 5.into_church());
-    assert_eq!(beta(app!(unwrap_or(), 5.into_church(), Some(1.into_church()).into_church()), HAP, 0), 1.into_church());
+    assert_eq!(beta(app!(unwrap_or(), 5.into_church(), none()), HAP, 0), 5.into_church());
+    assert_eq!(beta(app!(unwrap_or(), 5.into_church(), Some(1).into_church()), HAP, 0), 1.into_church());
 }
