28f73804c4a9da7a4220f26c42baf1b7945d2d3b
ljedrz <ljedrz@gmail.com>
2017-12-14 23:02:29 +0100

    fix 2 minor fails in docs

--- src/parigot/convert.rs
+++ src/parigot/convert.rs
@@ -4,6 +4,6 @@ use term::Term;
 
 /// Conversion to Parigot encoding
 pub trait IntoParigot {
-    /// Convert an object into a Church-encoded `Term`
+    /// Convert an object into a Parigot-encoded `Term`
     fn into_parigot(self) -> Term;
 }
--- src/scott/convert.rs
+++ src/scott/convert.rs
@@ -4,6 +4,6 @@ use term::Term;
 
 /// Conversion to Scott encoding
 pub trait IntoScott {
-    /// Convert an object into a Church-encoded `Term`
+    /// Convert an object into a Scott-encoded `Term`
     fn into_scott(self) -> Term;
 }
