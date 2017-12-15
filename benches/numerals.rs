82d587b6defbd5d418413e53e8ee1f3821cae288
ljedrz <ljedrz@gmail.com>
2017-12-15 16:44:16 +0100

    fill in the blanks in benches

--- benches/numerals.rs
+++ benches/numerals.rs
@@ -30,19 +30,19 @@ bench_num!(parigot, parigot_pred, into_parigot, pred, HAP, 1);
 bench_num!(stumpfu, stumpfu_pred, into_stumpfu, pred, HAP, 1);
 
 bench_num!(church,  church_add,  into_church,  add, HAP, 2, 2);
-
+//bench_num!(scott,   scott_add,   into_scott,  add, HAP, 2, 2);
 bench_num!(parigot, parigot_add, into_parigot, add, HAP, 2, 2);
 bench_num!(stumpfu, stumpfu_add, into_stumpfu, add, HAP, 2, 2);
 
 bench_num!(church,  church_sub,  into_church,  sub, HAP, 2, 2);
-
+//bench_num!(scott,   scott_sub,   into_scott,  sub, HAP, 2, 2);
 bench_num!(parigot, parigot_sub, into_parigot, sub, HAP, 2, 2);
-
+//bench_num!(stumpfu, stumpfu_sub, into_stumpfu, sub, HAP, 2, 2);
 
 bench_num!(church,  church_mult,  into_church,  mult, HAP, 2, 2);
-
+//bench_num!(scott,   scott_mult,   into_scott,   mult, HAP, 2, 2);
 bench_num!(parigot, parigot_mult, into_parigot, mult, HAP, 2, 2);
-
+//bench_num!(stumpfu, stumpfu_mult, into_stumpfu, mult, HAP, 2, 2);
 
 bench_num!(church, church_pow,  into_church, pow,  HAP, 2, 2);
 bench_num!(church, church_div,  into_church, div,  HAP, 2, 2);
