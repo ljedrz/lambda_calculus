fd349597d1fa7d2d2e2d8887ebecf4dd6212581d
ljedrz <ljedrz@gmail.com>
2017-12-15 12:59:26 +0100

    generate implementations with a macro

--- src/data/numerals/convert.rs
+++ src/data/numerals/convert.rs
@@ -4,7 +4,6 @@
 
 use term::{Term, abs, app};
 use term::Term::*;
-use data::boolean::{fls, tru};
 
 macro_rules! make_trait {
     ($trait_name:ident, $function_name:ident) => (
@@ -16,6 +15,9 @@ macro_rules! make_trait {
 }
 
 make_trait!(IntoChurch, into_church);
+make_trait!(IntoScott, into_scott);
+make_trait!(IntoParigot, into_parigot);
+make_trait!(IntoStumpFu, into_stumpfu);
 
 impl IntoChurch for usize {
     fn into_church(self) -> Term {
@@ -29,35 +31,6 @@ impl IntoChurch for usize {
     }
 }
 
-impl<T, U> IntoChurch for (T, U) where T: IntoChurch, U: IntoChurch {
-    fn into_church(self) -> Term {
-        abs(app!(Var(1), self.0.into_church(), self.1.into_church()))
-    }
-}
-
-impl<T> IntoChurch for Vec<T> where T: IntoChurch {
-    fn into_church(self) -> Term {
-        let mut ret = fls();
-
-        for term in self.into_iter().rev() {
-            ret = abs(app!(Var(1), term.into_church(), ret))
-        }
-
-        ret
-    }
-}
-
-impl<T> IntoChurch for Option<T> where T: IntoChurch {
-    fn into_church(self) -> Term {
-        match self {
-            None => tru(),
-            Some(value) => abs!(2, app(Var(1), value.into_church()))
-        }
-    }
-}
-
-make_trait!(IntoScott, into_scott);
-
 impl IntoScott for usize {
     fn into_scott(self) -> Term {
         let mut ret = abs!(2, Var(2));
@@ -69,8 +42,6 @@ impl IntoScott for usize {
         ret
     }
 }
-
-make_trait!(IntoParigot, into_parigot);
 
 impl IntoParigot for usize {
     fn into_parigot(self) -> Term {
@@ -84,8 +55,6 @@ impl IntoParigot for usize {
     }
 }
 
-make_trait!(IntoStumpFu, into_stumpfu);
-
 impl IntoStumpFu for usize {
     fn into_stumpfu(self) -> Term {
         let mut ret = abs!(2, Var(1));
@@ -97,3 +66,57 @@ impl IntoStumpFu for usize {
         ret
     }
 }
+
+macro_rules! impl_pair {
+    ($trait_name:ident, $function_name:ident) => (
+        impl<T, U> $trait_name for (T, U) where T: $trait_name, U: $trait_name {
+            fn $function_name(self) -> Term {
+                abs(app!(Var(1), (self.0).$function_name(), (self.1).$function_name()))
+            }
+        }
+    );
+}
+
+impl_pair!(IntoChurch, into_church);
+impl_pair!(IntoScott, into_scott);
+impl_pair!(IntoParigot, into_parigot);
+impl_pair!(IntoStumpFu, into_stumpfu);
+
+macro_rules! impl_list {
+    ($trait_name:ident, $function_name:ident) => (
+        impl<T> $trait_name for Vec<T> where T: $trait_name {
+            fn $function_name(self) -> Term {
+                let mut ret = abs!(2, Var(1));
+
+                for term in self.into_iter().rev() {
+                    ret = abs(app!(Var(1), term.$function_name(), ret))
+                }
+
+                ret
+            }
+        }
+    );
+}
+
+impl_list!(IntoChurch, into_church);
+impl_list!(IntoScott, into_scott);
+impl_list!(IntoParigot, into_parigot);
+impl_list!(IntoStumpFu, into_stumpfu);
+
+macro_rules! impl_option {
+    ($trait_name:ident, $function_name:ident) => (
+        impl<T> $trait_name for Option<T> where T: $trait_name {
+            fn $function_name(self) -> Term {
+                match self {
+                    None => abs!(2, Var(2)),
+                    Some(value) => abs!(2, app(Var(1), value.$function_name()))
+                }
+            }
+        }
+    );
+}
+
+impl_option!(IntoChurch, into_church);
+impl_option!(IntoScott, into_scott);
+impl_option!(IntoParigot, into_parigot);
+impl_option!(IntoStumpFu, into_stumpfu);
