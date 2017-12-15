833260318d315e97dda60a378eb009df2dc2be6e
ljedrz <ljedrz@gmail.com>
2017-12-15 12:39:19 +0100

    generate conversion traits with a macro

--- src/data/numerals/convert.rs
+++ src/data/numerals/convert.rs
@@ -1,27 +1,19 @@
 //! Conversions to different `Term` encodings
 
+#![allow(missing_docs)]
+
 use term::Term;
 
-/// Conversion to Church encoding
-pub trait IntoChurch {
-    /// Convert an object into a Church-encoded `Term`
-    fn into_church(self) -> Term;
+macro_rules! make_trait {
+    ($trait_name:ident, $function_name:ident) => (
+        pub trait $trait_name {
+            #[doc="Performs the conversion."]
+            fn $function_name(self) -> Term;
+        }
+    );
 }
 
-/// Conversion to Scott encoding
-pub trait IntoScott {
-    /// Convert an object into a Scott-encoded `Term`
-    fn into_scott(self) -> Term;
-}
-
-/// Conversion to Parigot encoding
-pub trait IntoParigot {
-    /// Convert an object into a Parigot-encoded `Term`
-    fn into_parigot(self) -> Term;
-}
-
-/// Conversion to Stump-Fu encoding
-pub trait IntoStumpFu {
-    /// Convert an object into a Stump-Fu-encoded `Term`
-    fn into_stumpfu(self) -> Term;
-}
+make_trait!(IntoChurch, into_church);
+make_trait!(IntoScott, into_scott);
+make_trait!(IntoParigot, into_parigot);
+make_trait!(IntoStumpFu, into_stumpfu);
