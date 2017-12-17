e02adfda053c4b265b7b05e44a1aea36008cc176
ljedrz <ljedrz@gmail.com>
2017-12-17 15:09:04 +0100

    make list integration tests test all encodings at once (WIP)

--- tests/list.rs
+++ tests/list.rs
@@ -4,96 +4,54 @@ extern crate lambda_calculus as lambda;
 
 use lambda::*;
 use lambda::data::list::*;
-use lambda::data::boolean::fls;
-use lambda::data::numerals::church::{add, is_zero};
+//use lambda::data::numerals::church::{add, is_zero};
 
-#[test]
-fn list_last() {
-    let list1 = || { vec![1].into_church() };
-    let list2 = || { vec![0, 1, 2, 3, 4].into_church() };
-
-    assert_eq!(beta(app(last(), nil()), HAP, 0), nil());
-    assert_eq!(beta(app(last(), list1()), HAP, 0), 1.into_church());
-    assert_eq!(beta(app(last(), list2()), HAP, 0), 4.into_church());
+macro_rules! test_list {
+    ($name:ident, $function:ident, $($($n:expr),+ => $result:expr),+) => (
+        #[test]
+        fn $name() {
+            $(assert_eq!(beta(app!($function(), $($n.into_church()),*), HAP, 0), $result.into_church());)*
+            $(assert_eq!(beta(app!($function(), $($n.into_scott()),*), HAP, 0), $result.into_scott());)*
+            $(assert_eq!(beta(app!($function(), $($n.into_parigot()),*), HAP, 0), $result.into_parigot());)*
+            $(assert_eq!(beta(app!($function(), $($n.into_stumpfu()),*), HAP, 0), $result.into_stumpfu());)*
+        }
+    );
 }
 
-#[test]
-fn list_init() {
-    let list1 = || { vec![0, 1, 2, 3, 4].into_church() };
-    let list2 = || { vec![0, 1, 2, 3].into_church() };
-    let list3 = || { vec![2, 3].into_church() };
-    let list4 = || { vec![2].into_church() };
+test_list!(list_last, last,
+    // TODO: test empty list?
+    vec![1] => 1,
+    vec![1, 2, 3] => 3
+);
 
-    assert_eq!(beta(app(init(), list1()), HAP, 0), list2());
-    assert_eq!(beta(app(init(), list3()), HAP, 0), list4());
-    assert_eq!(beta(app(init(), list4()), HAP, 0), nil());
-    assert_eq!(beta(app(init(), nil()), HAP, 0), nil());
-}
+test_list!(list_init, init,
+    // TODO: test singleton list?
+    vec![1, 2] => vec![1],
+    vec![1, 2, 3] => vec![1, 2]
+);
 
-#[test]
-fn list_zip() {
-    let l1 = || { vec![0].into_church() };
-    let l2 = || { vec![0, 1, 2].into_church() };
-    let l3 = || { vec![2, 1].into_church() };
-    let p1 = || { vec![(0, 0)].into_church() };
-    let p2 = || { vec![(0, 0), (1, 1), (2, 2)].into_church() };
-    let p3 = || { vec![(0, 2), (1, 1)].into_church() };
-
-    assert_eq!(beta(app!(zip(), nil(), nil()), HAP, 0), nil());
-    assert_eq!(beta(app!(zip(), nil(), l1()), HAP, 0), nil());
-    assert_eq!(beta(app!(zip(), l1(), nil()), HAP, 0), nil());
-    assert_eq!(beta(app!(zip(), l1(), l1()), HAP, 0), p1());
-    assert_eq!(beta(app!(zip(), l1(), l2()), HAP, 0), p1());
-    assert_eq!(beta(app!(zip(), l2(), l1()), HAP, 0), p1());
-    assert_eq!(beta(app!(zip(), l2(), l2()), HAP, 0), p2());
-    assert_eq!(beta(app!(zip(), l2(), l3()), HAP, 0), p3());
-}
-
-#[test]
-fn list_zip_with() {
-    let l1 = || { vec![1].into_church() };
-    let l2 = || { vec![2].into_church() };
-    let l3 = || { vec![1, 2, 3].into_church() };
-    let l4 = || { vec![2, 4, 6].into_church() };
-    let l5 = || { vec![3].into_church() };
-
-    assert_eq!(beta(app!(zip_with(), add(), nil(), nil()), HAP, 0), nil());
-    assert_eq!(beta(app!(zip_with(), add(), l1(), nil()), HAP, 0), nil());
-    assert_eq!(beta(app!(zip_with(), add(), nil(), l1()), HAP, 0), nil());
-    assert_eq!(beta(app!(zip_with(), abs!(2, Var(1)), l1(), l1()), HAP, 0), l1());
-    assert_eq!(beta(app!(zip_with(), add(), l1(), l1()), HAP, 0), l2());
-    assert_eq!(beta(app!(zip_with(), add(), l3(), l3()), HAP, 0), l4());
-    assert_eq!(beta(app!(zip_with(), add(), l4(), l1()), HAP, 0), l5());
-    assert_eq!(beta(app!(zip_with(), add(), l1(), l4()), HAP, 0), l5());
-    assert_eq!(beta(app!(zip_with(), fls(), l1(), l4()), HAP, 0), l2());
-    assert_eq!(beta(app!(zip_with(), fls(), l4(), l1()), HAP, 0), l1());
-}
-
-#[test]
-fn list_take() {
-    let l1 = || { vec![0].into_church() };
-    let l2 = || { vec![0, 1].into_church() };
-    let l3 = || { vec![0, 1, 2].into_church() };
-    let l4 = || { vec![0, 1, 2, 3].into_church() };
-
-    assert_eq!(beta(app!(take(), 5.into_church(), l4()), HAP, 0), l4());
-    assert_eq!(beta(app!(take(), 4.into_church(), l4()), HAP, 0), l4());
-    assert_eq!(beta(app!(take(), 3.into_church(), l4()), HAP, 0), l3());
-    assert_eq!(beta(app!(take(), 2.into_church(), l4()), HAP, 0), l2());
-    assert_eq!(beta(app!(take(), 1.into_church(), l4()), HAP, 0), l1());
-    assert_eq!(beta(app!(take(), 0.into_church(), l4()), HAP, 0), nil());
-    assert_eq!(beta(app!(take(), 1.into_church(), l1()), HAP, 0), l1());
-    assert_eq!(beta(app!(take(), 0.into_church(), l1()), HAP, 0), nil());
-    assert_eq!(beta(app!(take(), 1.into_church(), nil()), HAP, 0), nil());
-}
-
+test_list!(list_zip, zip,
+    // TODO: test empty list
+    vec![1], vec![1] => vec![(1, 1)],
+    vec![1, 2], vec![1] => vec![(1, 1)],
+    vec![1], vec![1, 2] => vec![(1, 1)],
+    vec![1, 2], vec![3, 4] => vec![(1, 3), (2, 4)]
+);
+/* TODO: figure out passing multiple functions (inconsistent lockstep iteration)
+test_list!(list_zip, zip_with, add,
+    // TODO: test empty list
+    vec![1], vec![1] => vec![2],
+    vec![1, 2], vec![1] => vec![2],
+    vec![1], vec![1, 2] => vec![2],
+    vec![1, 2], vec![3, 4] => vec![4, 6]
+);
 #[test]
 fn list_take_while() {
-    let l1 = || { vec![0, 0, 2, 3].into_church() };
-    let l2 = || { vec![0, 0].into_church() };
-    let l3 = || { vec![1, 4, 2, 3].into_church() };
-    let l4 = || { vec![0, 4, 0, 0].into_church() };
-    let l5 = || { vec![0].into_church() };
+    let l1 = || { vec![0, 0, 2, 3].into_scott() };
+    let l2 = || { vec![0, 0].into_scott() };
+    let l3 = || { vec![1, 4, 2, 3].into_scott() };
+    let l4 = || { vec![0, 4, 0, 0].into_scott() };
+    let l5 = || { vec![0].into_scott() };
 
     assert_eq!(beta(app!(take_while(), is_zero(), nil()), HAP, 0), nil());
     assert_eq!(beta(app!(take_while(), is_zero(), l1()), HAP, 0), l2());
@@ -101,3 +59,4 @@ fn list_take_while() {
     assert_eq!(beta(app!(take_while(), is_zero(), l3()), HAP, 0), nil());
     assert_eq!(beta(app!(take_while(), is_zero(), l4()), HAP, 0), l5());
 }
+*/
