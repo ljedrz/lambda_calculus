9ed22c11ef6bdac677f8e85f1c71d5a7e0c7223d
ljedrz <ljedrz@gmail.com>
2017-12-18 16:48:50 +0100

    add integration tests and placeholders for missing ones

--- tests/list.rs
+++ tests/list.rs
@@ -9,8 +9,8 @@ macro_rules! test_list {
     ($name:ident, $function:ident, $($($n:expr),+ => $result:expr),+) => (
         #[test]
         fn $name() {
-            $(assert_eq!(beta(app!($function(), $($n.into_church()),*), HAP, 0), $result.into_church());)*
-            $(assert_eq!(beta(app!($function(), $($n.into_scott()),*), HAP, 0), $result.into_scott());)*
+            $(assert_eq!(beta(app!($function(), $($n.into_church()),*), HAP, 0),  $result.into_church());)*
+            $(assert_eq!(beta(app!($function(), $($n.into_scott()),*), HAP, 0),   $result.into_scott());)*
             $(assert_eq!(beta(app!($function(), $($n.into_parigot()),*), HAP, 0), $result.into_parigot());)*
             $(assert_eq!(beta(app!($function(), $($n.into_stumpfu()),*), HAP, 0), $result.into_stumpfu());)*
         }
@@ -37,13 +37,51 @@ test_list!(list_head, head,
     vec![1, 2, 3] => 1
 );
 
-test_list!(list_last, last,
-    vec![1] => 1,
-    vec![1, 2, 3] => 3
+test_list!(list_tail, tail,
+    vec![1] => empty(),
+    vec![1, 2] => vec![2],
+    vec![1, 2, 3] => vec![2, 3]
 );
 
 test_list_enc!(list_length, length,
     empty() => 0,
+    vec![1] => 1,
+    vec![1, 2] => 2,
+    vec![1, 2, 3] => 3
+);
+
+// test_list_enc!(list_index, index,
+
+test_list!(list_reverse, reverse,
+    empty() => empty(),
+    vec![1] => vec![1],
+    vec![1, 2] => vec![2, 1],
+    vec![1, 2, 3] => vec![3, 2, 1]
+);
+
+// test_list_enc!(list_list, list,
+
+test_list!(list_append, append,
+    empty(), empty() => empty(),
+    empty(), vec![1] => vec![1],
+    vec![1], empty() => vec![1],
+    vec![1], vec![2] => vec![1, 2],
+    vec![1, 2], vec![3] => vec![1, 2, 3],
+    vec![1], vec![2, 3] => vec![1, 2, 3],
+    vec![1, 2], vec![3, 4] => vec![1, 2, 3, 4]
+);
+
+/* TODO: figure out passing multiple functions (inconsistent lockstep iteration) */
+
+// test_list!(list_map, map, add,
+
+// test_list!(list_foldl, foldl, add,
+
+// test_list!(list_foldr, foldr, add,
+
+// test_list!(list_filter, filter, is_zero,
+
+test_list!(list_last, last,
     vec![1] => 1,
     vec![1, 2] => 2,
     vec![1, 2, 3] => 3
@@ -64,6 +102,8 @@ test_list!(list_zip, zip,
     vec![1, 2], vec![3, 4] => vec![(1, 3), (2, 4)]
 );
 
+//test_list!(list_zip, zip_with, add,
+
 test_list_enc!(list_take, take,
     0, empty() => empty(),
     1, empty() => empty(),
@@ -75,26 +115,4 @@ test_list_enc!(list_take, take,
     3, vec![1, 2] => vec![1, 2]
 );
 
-/* TODO: figure out passing multiple functions (inconsistent lockstep iteration)
-test_list!(list_zip, zip_with, add,
-    // TODO: test empty list
-    vec![1], vec![1] => vec![2],
-    vec![1, 2], vec![1] => vec![2],
-    vec![1], vec![1, 2] => vec![2],
-    vec![1, 2], vec![3, 4] => vec![4, 6]
-);
-#[test]
-fn list_take_while() {
-    let l1 = || { vec![0, 0, 2, 3].into_scott() };
-    let l2 = || { vec![0, 0].into_scott() };
-    let l3 = || { vec![1, 4, 2, 3].into_scott() };
-    let l4 = || { vec![0, 4, 0, 0].into_scott() };
-    let l5 = || { vec![0].into_scott() };
-
-    assert_eq!(beta(app!(take_while(), is_zero(), nil()), HAP, 0), nil());
-    assert_eq!(beta(app!(take_while(), is_zero(), l1()), HAP, 0), l2());
-    assert_eq!(beta(app!(take_while(), is_zero(), l2()), HAP, 0), l2());
-    assert_eq!(beta(app!(take_while(), is_zero(), l3()), HAP, 0), nil());
-    assert_eq!(beta(app!(take_while(), is_zero(), l4()), HAP, 0), l5());
-}
-*/
+//test_list!(list_take_while, take_while, is_zero,
